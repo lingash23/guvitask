59b2f439af93743974e665f8ca5153ae82682277
lingash23 <lingash2000@gmail.com>
2024-06-17 17:22:57 +0530

    changes

--- day16/script.js
+++ day16/script.js
@@ -10,36 +10,25 @@ counter.style.color = "yellow"
 
 const message = document.createElement("h2")
 message.classList.add("heading")
-message.innerHTML = "10"
-message.style.fontSize = "10rem"
-
-setTimeout(() => {
-    message.innerHTML = "10"
+message.innerHTML = "5"
+message.style.fontSize = "8rem"
     setTimeout(() => {
-        message.innerHTML = "9"
-        setTimeout(() => {
-            message.innerHTML = "8"
+        message.innerHTML = "5"
             setTimeout(() => {
-                message.innerHTML = "7"
-                setTimeout(() => {
-                    message.innerHTML = "6"
+                message.innerHTML = "4"
                     setTimeout(() => {
-                        message.innerHTML = "5"
-                        setTimeout(() => {
-                            message.innerHTML = "4"
+                        message.innerHTML = "3"
                             setTimeout(() => {
-                                message.innerHTML = "3"
-                                setTimeout(() => {
-                                    message.innerHTML = "2"
+                                message.innerHTML = "2"
                                     setTimeout(() => {
                                         message.innerHTML = "1"
-                                        setTimeout(() => {
-                                            message.innerHTML = "0"
                                             setTimeout(() => {
-                                                message.innerHTML = "Happy Independence Day"
-                                                message.style.fontSize = "10rem"
-                                                counter.classList.add("col-md-12")
-                                                counter.classList.remove("offset-md-4")
+                                                message.innerHTML = "0"
+                                                    setTimeout(() => {
+                                                    message.innerHTML = "Happy Independence Day to Everyone"
+                                                    message.style.fontSize = "8rem"
+                                                    counter.classList.add("col-md-12")
+                                                    counter.classList.remove("offset-md-4")
                                             }, 1000);
                                         }, 1000);
                                     }, 1000);    
@@ -48,15 +37,9 @@ setTimeout(() => {
                             }, 1000);
                         }, 1000);
                     }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
 
 counter.appendChild(message)
 row.appendChild(counter)
 main_div.appendChild(row)
-
 
 document.body.append(main_div)
